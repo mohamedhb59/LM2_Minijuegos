1e9fbcc6895bd122083da7721b2197005a0306ee
mohamedhb59 <mohamedhb59@educastur.es>
2024-03-14 20:30:29 +0100

    Actualizacion 10

---
+++ traducciones.js
@@ -0,0 +1,18 @@
+var traducciones = {
+    "es": {
+        "titulo": 
+        "descripcion":
+    },
+    "en": {
+        "titulo":
+        "descripcion"
+    },
+    "fr":{
+        "titulo":
+        "descripcion":
+    },
+    "it":{
+        "titulo"
+        "descripcion":
+    }
+}
